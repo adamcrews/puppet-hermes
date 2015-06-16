709d951173fe2dc315669554eae002d934ceb550
Adam Crews <adamc@24640-MBPR.local>
2015-06-16 09:59:51 -0700

    Forgot to update spec tests

--- spec/classes/hermes_spec.rb
+++ spec/classes/hermes_spec.rb
@@ -2,5 +2,5 @@ require 'spec_helper'
 describe 'hermes' do
   it { should contain_package('hermes') }
   it { should contain_package('hermes').with_provider('compressed_app') }
-  it { should contain_package('hermes').with_source('https://s3.amazonaws.com/hermesmacapp/Hermes-1.2.5.zip') }
+  it { should contain_package('hermes').with_source('https://s3.amazonaws.com/hermesmacapp/Hermes-1.2.6.zip') }
 end
